50d71286dd9cf999556b5586b709aacc976a99fd
Eric McGuirk <eric.mcguirk@gmail.com>
2020-08-07 19:36:51 -0400

    Result of match printed to DOM

--- script.js
+++ script.js
@@ -15,61 +15,30 @@ buttons.forEach((button) => {
 });
 
 function playRound(e) {
+    const div = document.querySelector('#result');
+
     const computerSelection = computerPlay();
     const playerSelection = e.srcElement.id;
     if (playerSelection == computerSelection) {
-        console.log("It's a tie!");
+        div.textContent = "It's a tie!";
     } else if (playerSelection == 'rock') {
         if (computerSelection == 'scissors') {
-            console.log("Congratulations! You Win!");
+
+            div.textContent = "Congratulations! You Win!"
         } else {
-            console.log("You lose! Paper beats rock");
+            div.textContent = "You lose! Paper beats rock.";
+        }
+    } else if (playerSelection == 'paper') {
+        if (computerSelection == 'rock') {
+            div.textContent = "Congratulations! You Win!";
+        } else {
+            div.textContent = "You lose! Scissors beats paper";
+        }
+    } else if (playerSelection == 'scissors') {
+        if (computerSelection == 'paper') {
+            div.textContent = "Congratulations! You Win!"
+        } else {
+            div.textContent = "You lose! Rock beats scissors."
         }
     }
 }
-
-/* function playRound(playerSelection, computerSelection) {
-  if (playerSelection.toLowerCase() == computerSelection) {
-      return "It's a tie!"
-  } else if (playerSelection.toLowerCase() == "rock") {
-      if (computerSelection == "scissors") {
-          console.log("You Win! Rock beats scissors")
-          return true
-      } else {
-          console.log("You Lose! Paper beats rock")
-          return false
-      }
-  } else if (playerSelection.toLowerCase() == "scissors") {
-      if (computerSelection == "paper") {
-          console.log("You Win! Scissors beats paper")
-          return true
-      } else {
-          console.log("You Lose! Rock beats scissors")
-          return false
-      }
-  } else if (playerSelection.toLowerCase() == "paper") {
-      if (computerSelection == "rock") {
-          console.log("You Win! Paper beats rock")
-          return true
-      } else {
-          console.log("You Lose! Scissors beats paper")
-          return false
-      }
-  }
-} */
-
-/* function game() {
-  let playerScore = 0;
-  let computerScore = 0;
-    if (playRound(prompt("Enter rock, paper, or scissors"), computerPlay())) {
-        playerScore++
-    } else {
-        computerScore++
-    }
-
-  if (playerScore > computerScore) {
-      console.log("You win the game!")
-  } else {
-      console.log("You lose the game!")
-  }
-} */
